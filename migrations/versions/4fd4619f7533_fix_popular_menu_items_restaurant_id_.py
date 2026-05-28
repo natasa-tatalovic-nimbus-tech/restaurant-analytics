7f93cf2ef96e5c60fd28fc1e597e3c4c4109ab35
natasa-tatalovic-nimbus-tech <natasa.tatalovic@nimbus-tech.io>
2026-05-28 16:27:29 +0200

    fix: isort formatting

--- migrations/versions/4fd4619f7533_fix_popular_menu_items_restaurant_id_.py
+++ migrations/versions/4fd4619f7533_fix_popular_menu_items_restaurant_id_.py
@@ -8,8 +8,8 @@ Create Date: 2026-05-28 15:57:28.531394
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "4fd4619f7533"
